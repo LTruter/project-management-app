a80a7a77112f2f07fb5925417d7837bd527f466f
LTruter <lariotruter28@gmail.com>
2026-06-04 12:07:13 +0200

    Add SelectedProject component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const PROJECT_ACTIONS = {
@@ -26,12 +27,21 @@ function App() {
     });
   }
 
-  // Cancels the creation of a new project
-  function handleCancelAddProject() {
+  // Displays selected project if a matching project id is found
+  function handleSelectProject(id) {
+    const selectedProject = projectsState.projects.find(
+      (project) => project.id === id,
+    );
+
+    const projectAction = selectedProject
+      ? PROJECT_ACTIONS.SELECTED
+      : PROJECT_ACTIONS.NONE_SELECTED;
+
     setProjectsState((prevState) => {
       return {
         ...prevState,
-        projectAction: PROJECT_ACTIONS.NONE_SELECTED,
+        selectedProject: selectedProject,
+        projectAction: projectAction,
       };
     });
   }
@@ -50,23 +60,38 @@ function App() {
     });
   }
 
-  console.log(projectsState.projects);
+  // Cancels the creation of a new project
+  function handleCancelAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        projectAction: PROJECT_ACTIONS.NONE_SELECTED,
+      };
+    });
+  }
 
-  const content =
-    projectsState.projectAction === PROJECT_ACTIONS.NONE_SELECTED ? (
-      <NoProjectSelected onCreateNewProject={handleCreateNewProject} />
-    ) : (
+  let content;
+
+  if (projectsState.projectAction === PROJECT_ACTIONS.SELECTED) {
+    content = <SelectedProject project={projectsState.selectedProject} />;
+  } else if (projectsState.projectAction === PROJECT_ACTIONS.CREATED) {
+    content = (
       <NewProject
         onAddNewPoject={handleAddProject}
         onCancelAddProject={handleCancelAddProject}
       />
     );
+  } else {
+    content = <NoProjectSelected onCreateNewProject={handleCreateNewProject} />;
+  }
 
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar
         onCreateNewProject={handleCreateNewProject}
+        onSelectedProject={handleSelectProject}
         projects={projectsState.projects}
+        selectedProjectId={projectsState.selectedProject?.id}
       />
       {content}
     </main>
